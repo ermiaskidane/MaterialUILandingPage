bdd23c16f85c9c5cafa4a61d0d20e6b3f436c080
ermias kidane <ermiaskidanegebre@gmail.com>
2022-08-23 20:30:33 +0100

    initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,103 @@
+import {
+  AppBar,
+  Toolbar,
+  styled,
+  Typography,
+  Box,
+  InputBase,
+  Badge,
+  Avatar,
+  Menu,
+  MenuItem,
+} from '@mui/material'
+import user1 from './assets/pexels-lair-arce-13174596.jpg'
+import { LogoDev, Mail, Notifications } from '@mui/icons-material'
+import React, { useState } from 'react'
+
+const StyledToolbar = styled(Toolbar)({
+  display: 'flex',
+  justifyContent: 'space-between',
+})
+
+const Search = styled('div')(({ theme }) => ({
+  backgroundColor: 'white',
+  padding: '0 10px',
+  borderRadius: theme.shape.borderRadius,
+  width: '40%',
+}))
+const Icons = styled(Box)(({ theme }) => ({
+  // backgroundColor: 'white',
+  display: 'none',
+  alignItems: 'center',
+  gap: '20px',
+  [theme.breakpoints.up('sm')]: {
+    display: 'flex',
+  },
+}))
+
+const UserBox = styled(Box)(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  gap: '10px',
+  [theme.breakpoints.up('sm')]: {
+    display: 'none',
+  },
+}))
+
+function Navbar() {
+  const [open, setOpen] = useState(false)
+  return (
+    <AppBar position='sticky'>
+      <StyledToolbar>
+        <Typography variant='h6' sx={{ display: { xs: 'none', sm: 'block' } }}>
+          ERI DEV
+        </Typography>
+        <LogoDev sx={{ display: { xs: 'block', sm: 'none' } }} />
+        <Search>
+          <InputBase placeholder='Search...' />
+        </Search>
+        <Icons>
+          <Badge badgeContent={4} color='error'>
+            <Mail />
+          </Badge>
+          <Badge badgeContent={2} color='error'>
+            <Notifications />
+          </Badge>
+          <Avatar
+            sx={{ width: 30, height: 30 }}
+            src={user1}
+            onClick={(e) => setOpen(true)}
+          />
+        </Icons>
+        <UserBox>
+          <Avatar
+            sx={{ width: 30, height: 30 }}
+            src={user1}
+            onClick={(e) => setOpen(true)}
+          />
+          <Typography variant='span'>ariana</Typography>
+        </UserBox>
+      </StyledToolbar>
+      <Menu
+        id='demo-positioned-menu'
+        aria-labelledby='demo-positioned-button'
+        open={open}
+        onClick={(e) => setOpen(false)}
+        anchorOrigin={{
+          vertical: 'top',
+          horizontal: 'right',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'right',
+        }}
+      >
+        <MenuItem>Profile</MenuItem>
+        <MenuItem>My account</MenuItem>
+        <MenuItem>Logout</MenuItem>
+      </Menu>
+    </AppBar>
+  )
+}
+
+export default Navbar
