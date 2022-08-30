aea3d13b9a194eead238ea59ee4737d3c744c701
Ermias <35461600+ermiaskidane@users.noreply.github.com>
2022-08-30 20:41:10 +0100

    Update Add.jsx

--- src/components/Add.jsx
+++ src/components/Add.jsx
@@ -1,11 +1,45 @@
-import { Fab, Tooltip } from '@mui/material'
-import React from 'react'
-import { Add as AddIcon } from '@mui/icons-material'
+import {
+  Avatar,
+  Box,
+  Button,
+  ButtonGroup,
+  Fab,
+  Modal,
+  Stack,
+  styled,
+  TextField,
+  Tooltip,
+  Typography,
+} from '@mui/material'
+import React, { useState } from 'react'
+import {
+  Add as AddIcon,
+  DateRange,
+  EmojiEmotions,
+  Image,
+  PersonAdd,
+  VideoCameraBack,
+} from '@mui/icons-material'
+
+const StyledModal = styled(Modal)({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+})
+
+const UserBox = styled(Box)({
+  display: 'flex',
+  alignItems: 'center',
+  gap: '10px',
+  marginBottom: '20px',
+})
 
 function Add() {
+  const [open, setOpen] = useState(false)
   return (
     <>
       <Tooltip
+        onClick={(e) => setOpen(true)}
         title='Delete'
         sx={{
           position: 'fixed',
@@ -17,6 +51,58 @@ function Add() {
           <AddIcon />
         </Fab>
       </Tooltip>
+      <StyledModal
+        open={open}
+        onClose={(e) => setOpen(false)}
+        aria-labelledby='modal-modal-title'
+        aria-describedby='modal-modal-description'
+      >
+        <Box
+          width={400}
+          height={280}
+          bgcolor={'background.default'}
+          color={'text.primary'}
+          p={3}
+          borderRadius={5}
+        >
+          <Typography variant='h6' color='gray' textAlign='center'>
+            Create Post
+          </Typography>
+          <UserBox>
+            <Avatar
+              src='https://images.pexels.com/photos/846741/pexels-photo-846741.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
+              sx={{ width: 30, height: 30 }}
+            />
+            <Typography fontWeight={500} variant='span'>
+              John Doe
+            </Typography>
+          </UserBox>
+          <TextField
+            sx={{ width: '100%' }}
+            id='standard-multiline-static'
+            multiline
+            rows={3}
+            placeholder="what's on your mind"
+            variant='standard'
+          />
+          <Stack direction='row' gap={1} mt={2} mb={3}>
+            <EmojiEmotions color='primary' />
+            <Image color='secondary' />
+            <VideoCameraBack color='success' />
+            <PersonAdd color='error' />
+          </Stack>
+          <ButtonGroup
+            fullWidth
+            variant='contained'
+            aria-label='outlined primary button group'
+          >
+            <Button>Post</Button>
+            <Button sx={{ width: '100px' }}>
+              <DateRange />
+            </Button>
+          </ButtonGroup>
+        </Box>
+      </StyledModal>
     </>
   )
 }
