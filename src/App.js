d311feca4da4afa4547bc16dc5970e6c5af41530
Ermias <35461600+ermiaskidane@users.noreply.github.com>
2022-08-30 20:39:53 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,11 @@
-import { Box, Container, Stack } from '@mui/material'
+import {
+  Box,
+  Container,
+  createTheme,
+  Stack,
+  ThemeProvider,
+} from '@mui/material'
+import { useState } from 'react'
 import Add from './components/Add'
 import Feed from './components/Feed'
 import Navbar from './components/Navbar'
@@ -6,12 +13,18 @@ import Rightbar from './components/Rightbar'
 import Sidebar from './components/Sidebar'
 
 function App() {
+  const [mode, setMode] = useState('dark')
+  const darkTheme = createTheme({
+    palette: {
+      mode: mode,
+    },
+  })
   return (
   <ThemeProvider theme={darkTheme}>
     <Box bgcolor={'background.default'} color={'text.primary'}>
       <Navbar />
       <Stack direction='row' spacing={2} justifyContent='space-between'>
-        <Sidebar />
+        <Sidebar setMode={setMode} mode={mode} />
         <Feed />
         <Rightbar />
       </Stack>
